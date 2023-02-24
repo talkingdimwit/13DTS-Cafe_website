61b2ca12a27e49822a37e7fa31cf390d7b9a24cd
talkingdimwit <19037@wc.school.nz>
2023-02-24 14:09:28 +1300

    added all files, next step is adding database

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def render_homepage():  # put application's code here
+    return render_template('home.html')
 
+@app.route('/menu')
+def render_menu_page():  # put application's code here
+    return render_template('menu.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/contact')
+def render_contact_page():  # put application's code here
+    return render_template('contact.html')
+
+app.run(host='0.0.0.0', debug=True)
